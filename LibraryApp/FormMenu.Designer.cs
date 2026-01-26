9d824abbeac29e8aeff36ff20ed1d4ce4f71a2d9
marrevn <sveta.i.volhina@gmail.com>
2026-01-26 13:16:07 +0500

    Создание формы FormMenu

---
+++ LibraryApp/FormMenu.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace LibraryApp
+{
+    partial class FormMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pnTop = new Panel();
+            button1 = new Button();
+            lblUserName = new Label();
+            btnLogin = new Button();
+            panel1 = new Panel();
+            btnProducts = new Button();
+            pnTop.SuspendLayout();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pnTop
+            // 
+            pnTop.Controls.Add(button1);
+            pnTop.Controls.Add(lblUserName);
+            pnTop.Controls.Add(btnLogin);
+            pnTop.Dock = DockStyle.Top;
+            pnTop.Location = new Point(0, 0);
+            pnTop.Margin = new Padding(4, 4, 4, 4);
+            pnTop.Name = "pnTop";
+            pnTop.Padding = new Padding(13, 0, 13, 13);
+            pnTop.Size = new Size(520, 51);
+            pnTop.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.FromArgb(74, 111, 165);
+            button1.Dock = DockStyle.Left;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(13, 0);
+            button1.Margin = new Padding(4, 4, 4, 4);
+            button1.Name = "button1";
+            button1.Size = new Size(150, 38);
+            button1.TabIndex = 9;
+            button1.Text = "Назад";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // lblUserName
+            // 
+            lblUserName.AutoSize = true;
+            lblUserName.Dock = DockStyle.Right;
+            lblUserName.Location = new Point(312, 0);
+            lblUserName.Margin = new Padding(4, 0, 4, 0);
+            lblUserName.Name = "lblUserName";
+            lblUserName.Size = new Size(45, 19);
+            lblUserName.TabIndex = 8;
+            lblUserName.Text = "label1";
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = Color.FromArgb(74, 111, 165);
+            btnLogin.Dock = DockStyle.Right;
+            btnLogin.FlatAppearance.BorderSize = 0;
+            btnLogin.FlatStyle = FlatStyle.Flat;
+            btnLogin.ForeColor = Color.White;
+            btnLogin.Location = new Point(357, 0);
+            btnLogin.Margin = new Padding(4, 4, 4, 4);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(150, 38);
+            btnLogin.TabIndex = 7;
+            btnLogin.Text = "Выход";
+            btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(btnProducts);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 51);
+            panel1.Name = "panel1";
+            panel1.Padding = new Padding(10);
+            panel1.Size = new Size(520, 294);
+            panel1.TabIndex = 3;
+            // 
+            // btnProducts
+            // 
+            btnProducts.BackColor = Color.AliceBlue;
+            btnProducts.Dock = DockStyle.Top;
+            btnProducts.FlatStyle = FlatStyle.Flat;
+            btnProducts.Font = new Font("Times New Roman", 12F);
+            btnProducts.ForeColor = Color.Black;
+            btnProducts.Location = new Point(10, 10);
+            btnProducts.Margin = new Padding(4);
+            btnProducts.Name = "btnProducts";
+            btnProducts.Size = new Size(500, 50);
+            btnProducts.TabIndex = 5;
+            btnProducts.Text = "КНИГИ";
+            btnProducts.UseVisualStyleBackColor = false;
+            // 
+            // FormMenu
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(520, 345);
+            Controls.Add(panel1);
+            Controls.Add(pnTop);
+            Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "FormMenu";
+            Text = "FormMenu";
+            pnTop.ResumeLayout(false);
+            pnTop.PerformLayout();
+            panel1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel pnTop;
+        private Button button1;
+        private Label lblUserName;
+        private Button btnLogin;
+        private Panel panel1;
+        private Button btnProducts;
+    }
+}
